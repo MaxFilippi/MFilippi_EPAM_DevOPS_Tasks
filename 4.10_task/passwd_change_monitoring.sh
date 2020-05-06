b5298f32dd5fc584a954189e3a64d7889375d8ba
MaxFilippi <max.filippi@gmail.com>
2020-05-06 20:24:28 +0300

    Script is Updated

---
+++ 4.10_task/passwd_change_monitoring.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#cp  passwd file for  compare
+cat /etc/passwd > /tmp/old.passwd
+
+while :
+ do
+ #check SUM for find difference  
+ old=`cksum /tmp/old.passwd | awk {'print $1'}`
+ actual=`cksum /etc/passwd | awk {'print $1'}`
+ #if  difference is  presend -  add to  file
+ if [ $old != $actual ]
+ then
+   date >> /tmp/pwd.changes
+   echo "----" >> /tmp/pwd.changes
+   diff  /tmp/old.passwd /etc/passwd |  grep ":"| awk -F: {'print  $1 " " $3 '} >> /tmp/pwd.changes 
+   echo "----" >> /tmp/pwd.changes
+ fi
+ #update old pwd file for  next  checking
+ cat /etc/passwd > /tmp/old.passwd
+ #wait 1 min
+ sleep 60
+done
